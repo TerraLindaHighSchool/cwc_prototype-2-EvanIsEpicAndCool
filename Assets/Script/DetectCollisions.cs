1f4c54f5b2f2c2dabd8bb39e7d9d8c8d30e087eb
Evan Egan <ee42425@srcs.org>
2022-04-17 20:18:15 -0700

    accidently forgot to commit as i finished
    whoops (doing the challenge)

---
+++ Assets/Script/DetectCollisions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects if projectiles collide and destroys both.
+/// </summary>
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
+    
+}
